54edb82fcc0c9a22782902317d6f83bffd721ca3
Juan Fernandes <juanlima0604@gmail.com>
2024-05-27 20:32:52 -0300

    novo projeto

---
+++ projeto/src/components/Post.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react"
+import { StyleSheet, View, Image, Dimensions } from "react-native";
+import Author from "./Author";
+
+class Post extends Component {
+    render(){
+        return (
+            <View style={styles.container}>
+                <Image source={this.props.image} style={styles.image}/>
+                <Author email='fulanodetal@gmail.com' nickname='Teodor NickFail'/>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+    },
+    image: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').width * 3/4,
+        resizeMode: 'contain'
+    }
+})
+
+export default Post
