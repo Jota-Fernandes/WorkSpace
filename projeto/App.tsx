66dac4e1cce5f69a71d160ade9a40f5dbf7721c8
Juan Fernandes <juanlima0604@gmail.com>
2024-05-29 10:05:56 -0300

    mais alteracoes

--- projeto/App.tsx
+++ projeto/App.tsx
@@ -1,17 +1,23 @@
 import React, {Component} from 'react';
 import Header from './src/components/Header';
-import {View, Text} from 'react-native'
+import {View} from 'react-native'
 import Post from './src/components/Post'
 
 export default class App extends Component{
   render(){
+    const comments = [{
+      nickname: 'Joana Elena Silva',
+      comment: 'Excelente Foto'
+    },{
+      nickname: 'Rafael Gustavo Pereira',
+      comment: 'Muito ruim! Faça melhor...'
+    }]
     return(
       <View style={{flex:1}}>
         <Header/>
-        <Post image={require('./assets/imgs/bradesco.jpg')}/>
+        <Post image={require('./assets/imgs/bradesco.jpg')}
+          comments={comments}/>
       </View>
     )
   }
 }
-
-
