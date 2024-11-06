55173a39f7a6a85f3de34a10907c2d4643729bdd
Juan Fernandes <juanlima0604@gmail.com>
2024-11-06 20:15:54 -0300

    Criado projeto parelelo para testar o banco

---
+++ Rocketseat/realmgithub/src/pages/Main/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Main = () =>{
+    return(
+        <View>
+            <Text>Este é um componente Main</Text>
+        </View>
+    )
+}
+
+export default Main;
