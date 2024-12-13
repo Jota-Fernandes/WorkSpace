95f549f869a03e720f28f81ce716806ed8dac38e
Juan Fernandes <juanlima0604@gmail.com>
2024-12-13 20:44:18 -0300

    .

--- College/5TAV/tic_tac_toe/main4.py
+++ College/5TAV/tic_tac_toe/main4.py
@@ -3,16 +3,18 @@ import matplotlib.pyplot as plt
 
 Q = {}
 epsilon = 0.9
-epsilon_decay = 0.995 
+epsilon_min = 0.01
+epsilon_decay = 0.995
 learning_rate = 0.1
-discount_factor = 0.9
+discount_factor = 0.95
 
 def atualizar_q_table(estado, acao, recompensa, novo_estado):
     if estado not in Q:
         Q[estado] = [0] * 9
     if novo_estado not in Q:
         Q[novo_estado] = [0] * 9
-    Q[estado][acao] += learning_rate * (recompensa + discount_factor * max(Q[novo_estado]) - Q[estado][acao])
+    if acao is not None:
+        Q[estado][acao] += learning_rate * (recompensa + discount_factor * max(Q[novo_estado]) - Q[estado][acao])
 
 def imprimir_tabuleiro(tabuleiro): 
     for i in range(0, 9, 3): 
@@ -26,10 +28,10 @@ def maquina_jogar(jogador, tabuleiro, random_player=False):
     global epsilon
     if random_player or random.random() < epsilon:  # Taxa de exploração
         posicao = random.choice(posicoes_vazias)
-    else:  
+    else:  # Exploração de melhores jogadas
         posicoes_q = [Q.get(estado, [0] * 9)[i] for i in posicoes_vazias]
         posicao = posicoes_vazias[posicoes_q.index(max(posicoes_q))]
-    
+
     tabuleiro[posicao] = jogador
     return tabuleiro, posicao
 
@@ -48,6 +50,17 @@ def verificar_vencedor(jogador, tabuleiro):
 def verificar_empate(tabuleiro):
     return all(pos != ' ' for pos in tabuleiro)
 
+def prevenir_derrota(jogador_oponente, tabuleiro):
+    for i in range(9):
+        if tabuleiro[i] == ' ':
+            tabuleiro[i] = jogador_oponente
+            if verificar_vencedor(jogador_oponente, tabuleiro):
+                tabuleiro[i] = ' '
+                return i  # Retorna a posição que impede a derrota
+            tabuleiro[i] = ' '
+    return None
+
+# Inicialização
 tabuleiro = [' ' for _ in range(9)]
 jogador1 = 'X'
 jogador2 = 'O'
@@ -66,57 +79,57 @@ for i in range(qtdPartida):
     estado_anterior = None
     acao_anterior = None
     while True:
-        tabuleiro, posicao2 = maquina_jogar(jogador2, tabuleiro, random_player=True)
-        
-        #imprimir_tabuleiro(tabuleiro)
-        
+        # Jogada do jogador 1 (aleatório)
+        posicao1 = random.choice([i for i in range(9) if tabuleiro[i] == ' '])
+        tabuleiro[posicao1] = jogador1
+
         if verificar_vencedor(jogador1, tabuleiro):
+            derrota += 1
+            resultados.append('Derrota')
+            atualizar_q_table(estado_anterior, acao_anterior, -10, tuple(tabuleiro))
+            tabuleiro = [' ' for _ in range(9)]
+            break
+        elif verificar_empate(tabuleiro):
+            empate += 1
+            resultados.append('Empate')
+            atualizar_q_table(estado_anterior, acao_anterior, 0.5, tuple(tabuleiro))
+            tabuleiro = [' ' for _ in range(9)]
+            break
+
+        # Jogada do jogador 2 (inteligente)
+        posicao_bloqueio = prevenir_derrota(jogador1, tabuleiro)
+        if posicao_bloqueio is not None:
+            tabuleiro[posicao_bloqueio] = jogador2
+            posicao2 = posicao_bloqueio
+        else:
+            tabuleiro, posicao2 = maquina_jogar(jogador2, tabuleiro)
+
+        if verificar_vencedor(jogador2, tabuleiro):
             vitoria += 1
             resultados.append('Vitória')
-            if estado_anterior is not None:
+            if estado_anterior is not None and acao_anterior is not None:
                 atualizar_q_table(estado_anterior, acao_anterior, 1, tuple(tabuleiro)) 
             tabuleiro = [' ' for _ in range(9)]
             break
         elif verificar_empate(tabuleiro):
             empate += 1
             resultados.append('Empate')
-            if estado_anterior is not None:
+            if estado_anterior is not None and acao_anterior is not None:
                 atualizar_q_table(estado_anterior, acao_anterior, 0.5, tuple(tabuleiro))
             tabuleiro = [' ' for _ in range(9)]
             break
-        
-        if estado_anterior is not None:
+
+        if estado_anterior is not None and acao_anterior is not None:
             atualizar_q_table(estado_anterior, acao_anterior, 0, tuple(tabuleiro)) 
         estado_anterior = tuple(tabuleiro)
         acao_anterior = posicao2
-
-        #posicao do primeiro
-        tabuleiro, posicao1 = maquina_jogar(jogador1, tabuleiro)
-        #imprimir_tabuleiro(tabuleiro)
-
-        if verificar_vencedor(jogador2, tabuleiro):
-            derrota += 1
-            resultados.append('Derrota')
-            atualizar_q_table(estado_anterior, acao_anterior, -5, tuple(tabuleiro))  # Recompensa negativa para derrota
-            tabuleiro = [' ' for _ in range(9)]
-            break
-        elif verificar_empate(tabuleiro):
-            empate += 1
-            resultados.append('Empate')
-            atualizar_q_table(estado_anterior, acao_anterior, 0.5, tuple(tabuleiro))  # Recompensa neutra para empate
-            tabuleiro = [' ' for _ in range(9)]
-            break
-        
-        atualizar_q_table(estado_anterior, acao_anterior, 0, tuple(tabuleiro))
-        estado_anterior = tuple(tabuleiro)
-        acao_anterior = posicao1
 
     vitorias_acumuladas.append(vitoria)
     derrotas_acumuladas.append(derrota)
     empates_acumulados.append(empate)
     
     # Decay the exploration rate
-    epsilon = max(0.1, epsilon * epsilon_decay)
+    epsilon = max(epsilon_min, epsilon * epsilon_decay)
 
 # Cálculo das porcentagens
 porcentagem_vitoria = (vitoria / qtdPartida) * 100
